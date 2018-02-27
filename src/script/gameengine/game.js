828def9c975d9d0d99e6b732912cf600837fed33
Önder ALTINTAŞ <onderaltintas@gmail.com>
2018-02-27 22:08:20 +0300

    Jump like a mario, run with sprites
    Second commit:<br />
    27.02.2017<br />
    20:29 - 22:03 <br/>
    -Mario jump mechanics, have read a lot of articles and implemented half arsed. Will be in character class soon.<br/>
    -Movement sprite instead of loading animation frames one by one. Because it blows when you deploy it as website (downloading times)... Also helps performance.<br/>
    -Shaping up domain model.<br/>
    -deployed: http://159.89.177.161:8080/chibi-rpg/ <br/>

---
+++ src/script/gameengine/game.js
@@ -0,0 +1,146 @@
+var Game = function() {
+  var lastRender = 0;
+  var canvas = document.getElementById("canvas")
+  var width = canvas.width = window.innerWidth;
+  var height = canvas.height = window.innerHeight;
+  var ctx = canvas.getContext("2d")
+  var characterImage = new Image();
+  var backgroundImage = new Image();
+  var animState = 3;  
+  var jumpPower = 0;
+  var initialY = -999;
+  var jumpStep = 0;
+  var movementDirection = 0;
+  var state = {
+    x: (71),
+    y: (187),
+    pressedKeys: {
+      left: false,
+      right: false,
+      up: false,
+      down: false
+    }
+  }
+  
+  var keyMap = {
+    39: 'right',
+    37: 'left',
+    38: 'up',
+    40: 'down',
+    32: 'jump'
+  }
+  
+  function init(){
+    backgroundImage.src = 'media/background2.jpg';
+    characterImage.src = 'media/character1/move.png';
+    window.addEventListener("keydown", keydown, false)
+    window.addEventListener("keyup", keyup, false)
+  }
+ 
+  function keydown(event) {
+    console.log(event.keyCode);
+    var key = keyMap[event.keyCode]
+    state.pressedKeys[key] = true
+  }
+  
+  function keyup(event) {
+    var key = keyMap[event.keyCode]
+    state.pressedKeys[key] = false
+  }
+
+  function update(progress) {
+    movementDirection = 0;
+    var moved = false;
+    if (state.pressedKeys.left) {
+        moved = true;
+        state.x -= 1
+        movementDirection = -1;
+    }
+    if (state.pressedKeys.right) {
+      moved = true;
+      state.x += 1
+      movementDirection = +1;
+    }
+    if (state.pressedKeys.up) {
+      moved = true;
+      state.y -= 1
+    }
+    if (state.pressedKeys.down) {
+      moved = true;
+      state.y += 1
+    }
+    
+    if (state.pressedKeys.jump) {
+      if(initialY === -999){
+        initialY = state.y;
+      }
+      
+      if(jumpPower === -999) {
+        jumpPower = 20;
+      } else {
+        jumpPower = jumpPower + 1 > 30? 30 : jumpPower + 1;
+      }
+    }
+
+    if(initialY !== -999) 
+    {
+      jumpStep++;
+      jumpPower = jumpPower - 2;
+      state.y = state.y - jumpPower > initialY? initialY : state.y - jumpPower;
+      
+      state.y = state.y < 0 ? 0 : state.y;
+      //superjump// state.x = state.x + (initialY - state.y);
+      state.x = state.x + movementDirection * (initialY - state.y)/30;
+      if(state.y === initialY){
+        initialY = -999;
+        jumpPower = -999;
+        jumpStep = 0;
+      }
+    }
+    
+    if(state.y < 170 && jumpPower === -999) state.y = 170;
+    // Flip position at boundaries
+    if (state.x > width) {
+      state.x -= width
+    }
+    else if (state.x < 0) {
+      state.x += width
+    }
+    if (state.y > height) {
+      state.y -= height
+    }
+    else if (state.y < 0) {
+      state.y += height
+    }
+    
+    if(moved) animState = (animState+1)%17;
+    else animState = 0;
+    
+    document.getElementById('stateDiv').innerHTML = "x:"+state.x + " y:"+state.y;
+  }
+  
+  function draw() {
+    ctx.clearRect(0, 0, width, height);
+    ctx.drawImage(backgroundImage, -1 * state.x , -1 * state.y, canvas.width * 2, canvas.height * 2);
+    ctx.drawImage(characterImage, 0 , animState * 100, 100, 100, state.x, state.y, 100, 100);
+  }
+  
+  function loop(timestamp) {
+    var progress = timestamp - lastRender;
+    update(progress);
+    draw()
+    lastRender = timestamp
+    window.requestAnimationFrame(loop)
+  }
+
+  this.run = function(){ 
+    init();
+    window.requestAnimationFrame(loop)
+  }
+  
+  var self = this;
+}
+
+var game = new Game();
+game.run();
+
